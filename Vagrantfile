b38618caf70995ba78446ce4bbc7e73aa635ea4e
modernfidelity <mark@modernfidelity.co.uk>
2013-02-09 11:31:06 +0000

    very basic one box installing nginx,apache,mysql,varnish via puppet

--- Vagrantfile
+++ Vagrantfile
@@ -1,16 +1,25 @@
 Vagrant::Config.run do |config|
 
 
-
   # LOAD BALANCER
+  # --------------------------------------
 
   config.vm.define :lb do |config|
     
     config.vm.box = "precise64"
+    #config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+    
     config.vm.host_name = "lb.local"
     config.vm.network :hostonly, "33.33.33.30"
     config.vm.forward_port(80,3030)
 
+    # Set the Timezone to something useful
+    # config.vm.provision :shell, :inline => "echo \"Europe/London\" | sudo tee /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata"
+    
+    # Update the server
+    config.vm.provision :shell, :inline => "apt-get update --fix-missing"
+
+   
     config.vm.provision :puppet, :module_path => "puppet/modules" do |puppet|
       puppet.manifests_path = "puppet/manifests"
       puppet.manifest_file = "default.pp"
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,5 +1,9 @@
+
 # Basic Puppet Apache manifest
 
+
+# PUPPET
+# --------------------------
 
 class groups {
 
@@ -9,6 +13,8 @@ class groups {
 
 }
 
+# APACHE
+# --------------------------
 
 class apache {
 
@@ -44,6 +50,8 @@ class apache {
 
 }
 
+# PHP
+# --------------------------
 
 class php {
 
@@ -65,6 +73,9 @@ class php {
 }
  
 
+# NGINX
+# --------------------------
+
 class nginx {
  
   package { "nginx":
@@ -73,6 +84,10 @@ class nginx {
 
 
 }
+
+
+# VARNISH
+# --------------------------
 
 class varnish {
  
@@ -84,12 +99,11 @@ class varnish {
 }
 
 
-
+# MySQL
+# --------------------------
 
 class mysql {
 
-
- 
   package {
     ["mysql-client", "mysql-server", "libmysqlclient-dev"]: 
       ensure => installed, 
@@ -110,14 +124,19 @@ class mysql {
  
   }
  
-  exec { "create-database":
-    unless  => "/usr/bin/mysql -usite_development -psite_development site_development",
-    command => "/usr/bin/mysql -uroot -proot -e \"create database site_development; grant all on site_development.* to site_dev@localhost identified by 'site_development';\"",
-    require => Service["mysql"],
-  }
+  #exec { "create-database":
+  #  unless  => "/usr/bin/mysql -usite_development -psite_development site_development",
+  #  command => "/usr/bin/mysql -uroot -proot -e \"create database site_development; grant all on site_development.* to site_dev@localhost identified by 'site_development';\"",
+  #  require => Service["mysql"],
+  #}
 
 
 }
+
+
+
+# --------------------------
+
 
 include groups
 include nginx
