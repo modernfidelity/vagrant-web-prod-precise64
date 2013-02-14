331470c333e655ec4f53cde59074d9f730634ad4
modernfidelity <mark@modernfidelity.co.uk>
2013-02-14 16:44:32 +0000

    moved mysql into puppet module

--- puppet/manifests/db.pp
+++ puppet/manifests/db.pp
@@ -35,48 +35,13 @@ class system-update {
 }
 
 
-
-
-
-# MySQL
-# --------------------------
-
-class mysql {
-
-  package {
-    ["mysql-client", "mysql-server", "libmysqlclient-dev"]: 
-      ensure => installed, 
-      
-  }
-
- 
-  service { "mysql":
-    ensure => running,
-    require => Package["mysql-server"],
-  }
- 
-  exec { "set-mysql-password":
-    unless  => "mysql -uroot -proot",
-    path    => ["/bin", "/usr/bin"],
-    command => "mysqladmin -uroot password root",
-    require => Service["mysql"],
- 
-  }
- 
-
-  #exec { "create-database":
-  #  unless  => "/usr/bin/mysql -usite_development -psite_development site_development",
-  #  command => "/usr/bin/mysql -uroot -proot -e \"create database site_development; grant all on site_development.* to site_dev@localhost identified by 'site_development';\"",
-  #  require => Service["mysql"],
-  #}
-
-
-}
-
-
-# --------------------------
-
-
 include groups
+
 include system-update
+
+
+
+# MySQL (from module)
+# --------------------------
+
 include mysql
